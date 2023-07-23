f1c7210b4a2a05b90dedfa14b52a9c1f8157e590
rudi <rudi@bukuku.solutions>
2023-07-23 20:09:42 +0700

    build

---
+++ layouts/default.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="max-w-7xl mx-auto">
+    <nav class="bg-white border-gray-200 dark:bg-gray-900 text-md">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+<!--        <a href="https://flowbite.com/" class="flex items-center">-->
+<!--          <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo"/>-->
+          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">DiBumi</span>
+<!--        </a>-->
+        <button data-collapse-toggle="navbar-default" type="button"
+                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="navbar-default" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M1 1h15M1 7h15M1 13h15"/>
+          </svg>
+        </button>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a href="#"
+                 class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                 aria-current="page">Home</a>
+            </li>
+            <li>
+              <a href="#"
+                 class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Portofolio</a>
+            </li>
+            <li>
+              <a href="#"
+                 class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Opensource</a>
+            </li>
+            <li>
+              <a href="#"
+                 class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <div class="max-w-7xl mx-auto">
+    <slot/>
+  </div>
+
+
+<!--  <div id="informational-banner" tabindex="-1"-->
+<!--       class="fixed bottom-0 left-0 z-50 flex flex-col justify-between w-full p-4 border-b border-gray-200 md:flex-row bg-gray-50 dark:bg-gray-700 dark:border-gray-600">-->
+<!--    <div class="mb-4 md:mb-0 md:mr-4">-->
+<!--      <h2 class="mb-1 text-base font-semibold text-gray-900 dark:text-white">Special Offer !!!</h2>-->
+<!--      <p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">Take Advantage of Our Premium Web Development Services - Unleash Your Digital Potential Now!</p>-->
+<!--    </div>-->
+<!--    <div class="flex items-center flex-shrink-0">-->
+<!--      <a href="#"-->
+<!--         class="inline-flex items-center justify-center px-3 py-2 mr-3 text-xs font-medium text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">-->
+<!--        <svg class="w-3 h-3 mr-2 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"-->
+<!--             viewBox="0 0 20 18">-->
+<!--          <path-->
+<!--              d="M9 1.334C7.06.594 1.646-.84.293.653a1.158 1.158 0 0 0-.293.77v13.973c0 .193.046.383.134.55.088.167.214.306.366.403a.932.932 0 0 0 .5.147c.176 0 .348-.05.5-.147 1.059-.32 6.265.851 7.5 1.65V1.334ZM19.707.653C18.353-.84 12.94.593 11 1.333V18c1.234-.799 6.436-1.968 7.5-1.65a.931.931 0 0 0 .5.147.931.931 0 0 0 .5-.148c.152-.096.279-.235.366-.403.088-.167.134-.357.134-.55V1.423a1.158 1.158 0 0 0-.293-.77Z"/>-->
+<!--        </svg>-->
+<!--        Apply Project</a>-->
+<!--      <a href="#"-->
+<!--         class="inline-flex items-center justify-center px-3 py-2 mr-2 text-xs font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">-->
+<!--        See Portfolio-->
+<!--        <svg class="w-3 h-3 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">-->
+<!--          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
+<!--                d="M1 5h12m0 0L9 1m4 4L9 9"/>-->
+<!--        </svg>-->
+<!--      </a>-->
+<!--      <button data-dismiss-target="#informational-banner" type="button"-->
+<!--              class="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">-->
+<!--        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">-->
+<!--          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"-->
+<!--                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>-->
+<!--        </svg>-->
+<!--        <span class="sr-only">Close banner</span>-->
+<!--      </button>-->
+<!--    </div>-->
+<!--  </div>-->
+
+  <footer class="m-4 max-w-7xl mx-auto">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+      <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 Dibumi. All Rights Reserved.</span>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,254 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Creating, Coding, and Conquering the Web',
+  ogTitle: 'Creating, Coding, and Conquering the Web',
+  description: 'Creating, Coding, and Conquering the Web',
+  ogDescription: 'Creating, Coding, and Conquering the Web.',
+});
+</script>
+
+<template>
+  <section class="bg-white dark:bg-gray-900">
+    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-28 lg:grid-cols-12">
+      <div class="mr-auto place-self-center lg:col-span-7">
+        <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+          Creating, Coding, and Conquering the Web
+        </h1>
+        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+<!--          Web Developer, Backend Engineer, System Administrator And Digital Marketing Spesialist.-->
+          Unleashing digital potential through expert web development. Crafting innovative solutions for your online success.
+        </p>
+        <a href="#"
+           class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+          See Portfolios
+          <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"></path>
+          </svg>
+        </a>
+        <a href="#"
+           class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+          Contact Us
+        </a>
+      </div>
+      <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+<!--        <img src="/img/hero.svg" class="" alt="mockup">-->
+        <img src="/img/hero-top.svg" class="" alt="mockup">
+<!--        <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup">-->
+      </div>
+    </div>
+  </section>
+
+  <section class="bg-white dark:bg-gray-900">
+    <div class="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
+      <h2 class="mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-4xl">Presenting Our Rock-Solid Tech Stack!</h2>
+      <div class="grid grid-cols-2 gap-8 text-gray-500 sm:gap-12 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
+        <a href="#" class="flex justify-center items-center">
+          <img src="/images/laravel.svg" alt="Laravel" class="h-9 hover:text-gray-900 dark:hover:text-white">
+        </a>
+        <a href="#" class="flex justify-center items-center">
+          <img src="/images/nuxtjs.svg" alt="Laravel" class="h-9 hover:text-gray-900 dark:hover:text-white">
+        </a>
+        <a href="#" class="flex justify-center items-center">
+          <img src="/images/flutter.svg" alt="Laravel" class="h-9 hover:text-gray-900 dark:hover:text-white">
+        </a>
+        <a href="#" class="flex justify-center items-center">
+          <img src="/images/tailwindcss-logotype.svg" alt="Laravel" class="h-9 hover:text-gray-900 dark:hover:text-white">
+        </a>
+        <a href="#" class="flex justify-center items-center">
+          <img src="/images/filament.svg" alt="Laravel" class="h-9 hover:text-gray-900 dark:hover:text-white">
+        </a>
+        <a href="#" class="flex justify-center items-center">
+          <img src="/images/laravel-octane.svg" alt="Laravel" class="h-9 hover:text-gray-900 dark:hover:text-white">
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <section class="bg-white dark:bg-gray-900 antialiased">
+    <div class="max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-16 lg:py-24">
+      <div class="max-w-2xl mx-auto text-center">
+        <h2 class="text-3xl font-extrabold leading-tight tracking-tight text-gray-900 sm:text-4xl dark:text-white">
+          Our Portfolio
+        </h2>
+        <p class="mt-4 text-base font-normal text-gray-500 sm:text-xl dark:text-gray-400">
+          Discover our diverse portfolio—captivating web apps, robust APIs, and seamless mobile solutions.        </p>
+      </div>
+
+      <div class="grid grid-cols-1 mt-12 text-center sm:mt-16 gap-x-20 gap-y-12 sm:grid-cols-2 lg:grid-cols-3">
+        <div class="space-y-4">
+        <span
+            class="bg-gray-100 text-gray-900 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+          www.kerupiah.com
+        </span>
+          <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+            Website & Payment System
+          </h3>
+          <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+            KeRupiah directly helps you bridge IDR (Rupiah) with Global Cryptocurrency ecosystem.
+          </p>
+          <a href="#" title=""
+             class="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+             role="button">
+            View case study
+            <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                 fill="currentColor">
+              <path fill-rule="evenodd"
+                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd" />
+            </svg>
+          </a>
+        </div>
+
+        <div class="space-y-4">
+        <span
+            class="bg-gray-100 text-gray-900 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+          www.bayarcepat.com
+        </span>
+          <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+            Multi Tenance System
+          </h3>
+          <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+            BayarCepat helps you sell digital product with ease. Connect to hundred of vendor easy like a god.
+          </p>
+          <a href="#" title=""
+             class="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+             role="button">
+            View case study
+            <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                 fill="currentColor">
+              <path fill-rule="evenodd"
+                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd" />
+            </svg>
+          </a>
+        </div>
+
+        <div class="space-y-4">
+        <span
+            class="bg-gray-100 text-gray-900 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">
+          www.finloan.id
+        </span>
+          <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+            Official Website
+          </h3>
+          <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+            Finloan is a one stop solution for all your financial needs. With this app, you can calculate KPR loan easy.
+          </p>
+          <a href="#" title=""
+             class="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+             role="button">
+            View case study
+            <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                 fill="currentColor">
+              <path fill-rule="evenodd"
+                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd" />
+            </svg>
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<!--  <section class="bg-white dark:bg-gray-900">-->
+<!--    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6">-->
+<!--      <div class="mx-auto max-w-screen-sm">-->
+<!--        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Testimonials</h2>-->
+<!--        <p class="mb-8 font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Explore the whole collection of open-source web components and elements built with the utility classes from Tailwind</p>-->
+<!--      </div>-->
+<!--      <div class="grid mb-8 lg:mb-12 lg:grid-cols-2">-->
+<!--        <figure class="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 lg:border-r dark:bg-gray-800 dark:border-gray-700">-->
+<!--          <blockquote class="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">-->
+<!--            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Speechless with how easy this was to integrate</h3>-->
+<!--            <p class="my-4">"I recently got my hands on Flowbite Pro, and holy crap, I'm speechless with how easy this was to integrate within my application. Most templates are a pain, code is scattered, and near impossible to theme.</p>-->
+<!--            <p class="my-4">Flowbite has code in one place and I'm not joking when I say it took me a matter of minutes to copy the code, customise it and integrate within a Laravel + Vue application.</p>-->
+<!--            <p class="my-4">If you care for your time, I hands down would go with this."</p>-->
+<!--          </blockquote>-->
+<!--          <figcaption class="flex justify-center items-center space-x-3">-->
+<!--            <img class="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture">-->
+<!--            <div class="space-y-0.5 font-medium dark:text-white text-left">-->
+<!--              <div>Bonnie Green</div>-->
+<!--              <div class="text-sm font-light text-gray-500 dark:text-gray-400">Developer at Open AI</div>-->
+<!--            </div>-->
+<!--          </figcaption>-->
+<!--        </figure>-->
+<!--        <figure class="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 md:p-12 dark:bg-gray-800 dark:border-gray-700">-->
+<!--          <blockquote class="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">-->
+<!--            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Solid foundation for any project</h3>-->
+<!--            <p class="my-4">"FlowBite provides a robust set of design tokens and components based on the popular Tailwind CSS framework. From the most used UI components like forms and navigation bars to the whole app screens designed both for desktop and mobile, this UI kit provides a solid foundation for any project.</p>-->
+<!--            <p class="my-4">Designing with Figma components that can be easily translated to the utility classes of Tailwind CSS is a huge timesaver!"</p>-->
+<!--          </blockquote>-->
+<!--          <figcaption class="flex justify-center items-center space-x-3">-->
+<!--            <img class="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/roberta-casas.png" alt="profile picture">-->
+<!--            <div class="space-y-0.5 font-medium dark:text-white text-left">-->
+<!--              <div>Roberta Casas</div>-->
+<!--              <div class="text-sm font-light text-gray-500 dark:text-gray-400">Lead designer at Dropbox</div>-->
+<!--            </div>-->
+<!--          </figcaption>-->
+<!--        </figure>-->
+<!--        <figure class="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-b border-gray-200 lg:border-b-0 md:p-12 lg:border-r dark:bg-gray-800 dark:border-gray-700">-->
+<!--          <blockquote class="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">-->
+<!--            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Mindblowing workflow and variants</h3>-->
+<!--            <p class="my-4">"As someone who mainly designs in the browser, I've been a casual user of Figma, but as soon as I saw and started playing with FlowBite my mind was 🤯.</p>-->
+<!--            <p class="my-4">Everything is so well structured and simple to use (I've learnt so much about Figma by just using the toolkit).</p>-->
+<!--            <p class="my-4">Aesthetically, the well designed components are beautiful and will undoubtedly level up your next application."</p>-->
+<!--          </blockquote>-->
+<!--          <figcaption class="flex justify-center items-center space-x-3">-->
+<!--            <img class="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="profile picture">-->
+<!--            <div class="space-y-0.5 font-medium dark:text-white text-left">-->
+<!--              <div>Jese Leos</div>-->
+<!--              <div class="text-sm font-light text-gray-500 dark:text-gray-400">Software Engineer at Facebook</div>-->
+<!--            </div>-->
+<!--          </figcaption>-->
+<!--        </figure>-->
+<!--        <figure class="flex flex-col justify-center items-center p-8 text-center bg-gray-50 border-gray-200 md:p-12 dark:bg-gray-800 dark:border-gray-700">-->
+<!--          <blockquote class="mx-auto mb-8 max-w-2xl text-gray-500 dark:text-gray-400">-->
+<!--            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Efficient Collaborating</h3>-->
+<!--            <p class="my-4">"This is a very complex and beautiful set of elements. Under the hood it comes with the best things from 2 different worlds: Figma and Tailwind.</p>-->
+<!--            <p class="my-4">You have many examples that can be used to create a fast prototype for your team."</p>-->
+<!--          </blockquote>-->
+<!--          <figcaption class="flex justify-center items-center space-x-3">-->
+<!--            <img class="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/joseph-mcfall.png" alt="profile picture">-->
+<!--            <div class="space-y-0.5 font-medium dark:text-white text-left">-->
+<!--              <div>Joseph McFall</div>-->
+<!--              <div class="text-sm font-light text-gray-500 dark:text-gray-400">CTO at Google</div>-->
+<!--            </div>-->
+<!--          </figcaption>-->
+<!--        </figure>-->
+<!--      </div>-->
+<!--      <div class="text-center">-->
+<!--        <a href="#" class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Show more...</a>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </section>-->
+
+  <section class="bg-white dark:bg-gray-900">
+    <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+      <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know. Or you can directly contact us at rudi@dibumi.com.</p>
+      <form action="#" class="space-y-8">
+        <div>
+          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+          <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="example: rudi@dibumi.com" required>
+        </div>
+        <div>
+          <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+          <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required>
+        </div>
+        <div class="sm:col-span-2">
+          <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+          <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+        </div>
+        <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+      </form>
+    </div>
+  </section>
+
+
+</template>
+
+<style scoped>
+
+</style>
